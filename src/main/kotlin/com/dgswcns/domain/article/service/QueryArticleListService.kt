512c0c15bd191e234f841c0db41acf9595048c53
pea06 <101560095+pea06@users.noreply.github.com>
2023-11-08 23:14:24 +0900

    refactor: article list by date with paging

--- src/main/kotlin/com/dgswcns/domain/article/service/QueryArticleListService.kt
+++ src/main/kotlin/com/dgswcns/domain/article/service/QueryArticleListService.kt
@@ -1,21 +1,18 @@
 package com.dgswcns.domain.article.service
 
-import com.dgswcns.domain.article.handler.dto.response.ArticleBodyResponse
-import com.dgswcns.domain.article.handler.dto.response.ArticleResponse
 import com.dgswcns.domain.article.handler.dto.response.ArticleSimpleResponse
-import com.dgswcns.domain.article.persistence.dao.ArticleRepository
 import com.dgswcns.domain.article.persistence.dao.CustomArticleRepository
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.toList
 import org.springframework.stereotype.Service
 
 @Service
 class QueryArticleListService(
-    private val articleRepository: ArticleRepository,
     private val customArticleRepository: CustomArticleRepository
 ) {
-    suspend fun execute(page: String): List<ArticleSimpleResponse> {
-        return customArticleRepository.findAllWithPaging(page)
+    suspend fun execute(
+        page: String,
+        date: String?
+    ): List<ArticleSimpleResponse> {
+        return customArticleRepository.findAllByDateWithPaging(page, date)
             .map {ArticleSimpleResponse(
                 it.id,
                 it.keyword,
