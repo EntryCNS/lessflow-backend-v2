51976cd6be54ac472e0b63b852473d00816473ab
pea06 <101560095+pea06@users.noreply.github.com>
2023-11-09 22:15:39 +0900

    feat: send article to mail

--- src/main/kotlin/com/dgswcns/domain/article/service/CreateArticleService.kt
+++ src/main/kotlin/com/dgswcns/domain/article/service/CreateArticleService.kt
@@ -7,37 +7,58 @@ import com.dgswcns.domain.article.persistence.entity.value.Body
 import com.dgswcns.domain.article.persistence.entity.value.Thumbnail
 import com.dgswcns.domain.article.persistence.entity.value.Video.Companion.invoke
 import com.dgswcns.global.util.NlpUtil
+import com.dgswcns.thirdparty.mail.SendMailService
+import com.dgswcns.thirdparty.news.GetLatestNewsService
 import com.dgswcns.thirdparty.news.SearchNewsService
 import com.dgswcns.thirdparty.shotstack.RenderVideoService
+import com.dgswcns.thirdparty.shotstack.webclient.dto.response.RenderVideoResponse
 import org.springframework.stereotype.Service
 
 @Service
 class CreateArticleService(
     private val searchNewsService: SearchNewsService,
+    private val getLatestNewsService: GetLatestNewsService,
     private val renderVideoService: RenderVideoService,
     private val nlpUtil: NlpUtil,
-    private val articleRepository: ArticleRepository
+    private val articleRepository: ArticleRepository,
+    private val sendMailService: SendMailService
 ) {
-    suspend fun create(request: CreateArticleRequest) {
+
+    suspend fun createByKeyword(request: CreateArticleRequest) {
         val news = searchNewsService.searchNewsByKeyword(request.keyword)
         val shortContents = shortContent(news.article.content)
-        val video = renderVideoService.execute(
-            imageUrl = news.article.thumbnail,
-            title = news.article.title,
-            keyword = news.keyword,
-            content =  shortContents
-        )
+        val video = invoke(renderVideo(news.article.thumbnail, news.article.title, news.keyword, shortContents).response.id)
         articleRepository.save(
             Article(
-                keyword = request.keyword,
+                keyword = news.keyword,
                 body = Body(
                     news.article.title,
-                    shortContents.joinToString("/n"),
+                    shortContents.joinToString("\n"),
                     Thumbnail(news.article.thumbnail)
                 ),
-                video = invoke(video.response.id)
+                video = video
             )
         )
+        sendMailService.send(
+            request.email,
+            "lessf!ow ${request.keyword} 키워드의 뉴스가 도착했어요!",
+            StringBuffer("<div style=\"text-align: center; width: 400px\">")
+                .append(" <p style=\"font-size: 2rem; font-weight: 700\">\n")
+                .append(news.article.title)
+                .append("</p>")
+                .append("<img src=\"${news.article.thumbnail}\"  style=\"height: 400px; width: 400px\" />\n")
+                .append("<p style=\"line-height: 30px; letter-spacing: 0.075rem; font-size: 1rem\">")
+                .append(shortContents.joinToString("\n"))
+                .append("</p>")
+                .append("영상 으로 보러 가기 -> $video")
+                .toString()
+        )
+
+    }
+
+
+    private suspend fun renderVideo(imageUrl: String, title: String, keyword: String, content: List<String>): RenderVideoResponse {
+        return renderVideoService.execute(imageUrl, title, keyword, content)
     }
 
     private suspend fun shortContent(content: String): List<String> {
